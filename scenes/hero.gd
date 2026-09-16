68c7f05ec038abca833d37c31212a2cc93911863
MrOnosa <supersnapple@gmail.com>
2026-09-16 07:43:34 -0500

    The hero appears

---
+++ scenes/hero.gd
@@ -0,0 +1,16 @@
+class_name Hero
+extends CharacterBody2D
+
+
+const SPEED = 700.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
